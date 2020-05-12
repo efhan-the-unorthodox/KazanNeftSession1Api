ef97bfca3e80f08c003484d569031d3beef8217a
efhan-the-unorthodox <1800249C@student.tp.edu.sg>
2020-05-12 22:16:49 +0800

    changes to api

--- routes/api.js
+++ routes/api.js
@@ -35,7 +35,8 @@ router.route("/getAssetGroups").post((req, res)=>{
 });
 
 router.route("/getAssets").post((req,res) =>{
-    db.query(`SELECT assets.ID,assets.AssetName, departments.Name as 'DepartmentName', assets.AssetSN, assetgroups.Name as 'AssetGroup' FROM assets 
+    db.query(`SELECT assets.ID,assets.AssetName, departments.Name as 'DepartmentName', 
+    assets.AssetSN, assetgroups.Name as 'AssetGroup' FROM assets 
     JOIN departmentlocations on DepartmentLocationID = departmentlocations.ID
     JOIN departments on departmentlocations.DepartmentID = departments.ID
     JOIN assetgroups on AssetGroupID = assetgroups.ID`, function(err, results){
@@ -56,5 +57,42 @@ router.route('/getLocations').post((req,res) =>{
     db.query(`SELECT * FROM locations`, function(err, results){
         if(err) throw err;
         res.send(results);
+    });
+});
+
+router.route('/departmentLocations').post((req, res)=>{
+    db.query(`SELECT * FROM departmentlocations`, (err, results)=>{
+        if(err) throw err;
+        res.send(results);
+    });
+});
+
+
+router.route('/newAsset').post((req,res)=>{
+    var AssetSN = req.body.AssetSN
+    var AssetName = req.body.AssetName;
+    var DepartmentLocationID = req.body.DepartmentLocationID;
+    var EmployeeID = req.body.EmployeeID;
+    var AssetGroupID = req.body.AssetGroupID;
+    var Description = req.body.Description;
+    var WarrantyDate = req.body.WarrantyDate;
+    console.log(AssetSN);
+
+    db.query(`INSERT INTO assets 
+    (ID, AssetSN, AssetName, 
+    DepartmentLocationID, EmployeeID, 
+    AssetGroupID, Description, WarrantyDate) 
+    VALUES (?,?,?,?,?,?,?,?)`,
+    [null,AssetSN, AssetName, DepartmentLocationID, EmployeeID,AssetGroupID, Description, WarrantyDate], 
+    function(error, results, fields){
+        if(error) throw error
+        if(results.affectedRows != 0){
+            console.log("New Asset Added");
+            res.send("New Asset Added");
+        }
+        else{
+            console.log("An Error Occured");
+            res.send("An Error Occured")
+        }
     });
 });
